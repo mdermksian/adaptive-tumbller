f1b85b3691e847f05b48f0231be5e0476dcb0103
Samuel Bednarski <samebednarski@gmail.com>
2020-11-21 15:57:04 -0500

    Removed build files from PIC firmware.
    Removed a bunch of garbage files that aren't important. Only remaining file is the C code.

--- Encoder Interface/EncoderInterface.X/build/default/production/_ext/1423095347/main.p1.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/1423095347/main.p1:  \
-C:/Users/bednarse/School/Fall\ 2020/ST\ Advanced\ Control\ Systems\ Integration/Project/Encoder\ Interface/EncoderInterface.X/main.c 
