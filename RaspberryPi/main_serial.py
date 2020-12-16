e37f37f0b67628114af14e18f2909d9da8582b7f
mdermksian <mdermksian@gmail.com>
2020-12-16 13:23:00 -0500

    Restructured slightly to separate adp and rls

--- RaspberryPi/main_serial.py
+++ RaspberryPi/main_serial.py
@@ -21,11 +21,12 @@ class Main:
         else:
             print("Please input a valid run mode")
             exit()
+        self.mode = mode
         self.cnt = 0
         self.ser = serial.Serial('/dev/ttyUSB0', self.BAUD, timeout=0.01) # <-- potentially address issues with timeout
         self.ser.flush()
-
-    def run(self):
+    
+    def run_rls(self):
         try:
             while True:
                 if self.ser.in_waiting > 0:
@@ -33,10 +34,10 @@ class Main:
                     if len(incoming) != self.incoming_size:
                         continue;
                     inp = struct.unpack(self.decode_string, incoming)
-                    print("Incoming:", inp)
+#                    print("Incoming:", inp)
                     K = self.program.main(inp, self.cnt)
                     self.cnt += 1
-                    print("Outgoing:", K)
+#                    print("Outgoing:", K)
                     outgoing = struct.pack("ffff", K[0], K[1], K[2], K[3])
                     self.ser.write(outgoing)
         except KeyboardInterrupt:
@@ -44,6 +45,26 @@ class Main:
         
         self.program.cleanup()
         print('Finishing')
+    
+    def run_adp(self):
+        try:
+            while True:
+                if self.ser.in_waiting > 0:
+                    incoming = self.ser.read(self.incoming_size)
+                    if len(incoming) != self.incoming_size:
+                        continue;
+                    inp = struct.unpack(self.decode_string, incoming)
+                    K = self.program.main(inp)
+                    outgoing = struct.pack("ffff", K[0], K[1], K[2], K[3])
+                    self.ser.write(outgoing)
+        except KeyboardInterrupt:
+            pass
+
+    def run(self):
+        if self.mode == "ADP":
+            self.run_adp()
+        elif self.mode == "RLS":
+            self.run_rls()
 
         
 if __name__ == "__main__":
